7b770b7768a0e79d9297e2e40f66cfd461fef9d5
Brinda bansal <bansalshikha487@gmail.com>
2023-08-04 22:01:46 +0530

    Add files via upload
    well this file is based on revering of arrays in c++, that too using functions

---
+++ reverseofarray.cpp
@@ -0,0 +1,54 @@
+//actually here do you notice that reversing the array is basically just swapping the values (pehla or aakhri, 2nd waale ka 2nd last waalese)
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n){
+int start=0;
+int end=n-1;
+while(start<=end)
+{
+    swap(arr[start],arr[end])
+    start++;
+    end--;
+}
+
+}
+
+
+void printArray(int arr[],int n)
+{
+   for(int i=0;i<n;i++){
+   cout<<arr[i]<<"";
+   }
+   cout<<endl;
+
+}
+
+
+int main()
+{
+int arr[6]={1.,4,8,5,-2,15};
+int brr[5]={2,6,3,9,4};
+
+
+reverse(arr,6);
+reverse(brr,5);
+
+printArray(arr,6);
+printArry(brr,5)
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
